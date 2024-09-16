d90facb968c1571b351958e4a3c69f138b3d24c2
ayushmishra333 <ayushmishra333@gmail.com>
2024-09-16 20:49:23 +0100

    fixes

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,3 +1,39 @@
+// /* eslint-disable prefer-const */
+// /* eslint-disable no-prototype-builtins */
+// /* eslint-disable no-unused-vars */
+// import mongoose, { Mongoose } from 'mongoose';
+
+// const MONGODB_URL = process.env.MONGODB_URL;
+
+// interface MongooseConnection {
+//   conn: Mongoose | null;
+//   promise: Promise<Mongoose> | null;
+// }
+
+// let cached: MongooseConnection = (global as any).mongoose
+
+// if(!cached) {
+//   cached = (global as any).mongoose = {
+//     conn: null, promise: null
+//   }
+// }
+
+// export const connectToDatabase = async () => {
+//   if(cached.conn) return cached.conn;
+
+//   if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+//   cached.promise =
+//     cached.promise ||
+//     mongoose.connect(MONGODB_URL, {
+//       dbName: 'vyzen', bufferCommands: false
+//     })
+
+//   cached.conn = await cached.promise;
+
+//   return cached.conn;
+// }
+
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-unused-vars */
@@ -10,26 +46,28 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+// Extend global to include the mongoose connection cache
+declare global {
+  var mongoose: MongooseConnection | undefined;
+}
 
-if(!cached) {
-  cached = (global as any).mongoose = {
-    conn: null, promise: null
-  }
+let cached: MongooseConnection = global.mongoose || { conn: null, promise: null };
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: 'vyzen', bufferCommands: false
-    })
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+    dbName: 'vyzen',
+    bufferCommands: false,
+  });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
